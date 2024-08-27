bf55ec78ec5171567bcbd9e759ae6244b5e19f19
Peyn <stmo.rendon.up@phinmaed.com>
2024-08-27 21:36:02 +0800

    JavaScanner

---
+++ ada.java/src/App.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        String name = s.nextLine();
+        System.out.println(name);
+
+    }
+}
